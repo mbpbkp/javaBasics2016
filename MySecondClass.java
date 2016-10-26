0ac728557cbe824f5fc33136ced400dc924ff29f
mbpbkp <mbp.bkp@gmail.com>
2016-10-25 22:57:42 -0400

    2ndClass

---
+++ MySecondClass.java
@@ -0,0 +1,26 @@
+package javaBasics2016;
+
+public class MySecondClass {
+ int height;
+ int weight;
+ int nothing;
+public int getHeight() {
+	return height;
+}
+public void setHeight(int height) {
+	this.height = height;
+}
+public int getWeight() {
+	return weight;
+}
+public void setWeight(int weight) {
+	this.weight = weight;
+}
+public int getNothing() {
+	return nothing;
+}
+public void setNothing(int nothing) {
+	this.nothing = nothing;
+}
+ 
+}
